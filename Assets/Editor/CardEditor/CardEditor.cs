5eaaff2ad79b08bbeada11e33182fc7065b3a6be
PixelVoid <Pixel0Void@gmail.com>
2025-05-07 14:42:23 +0430

    feat: create card level ups based on card type enum

--- Assets/Editor/CardEditor/CardEditor.cs
+++ Assets/Editor/CardEditor/CardEditor.cs
@@ -1,11 +1,18 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
+using System;
+using UnityEditor.UIElements;
+using System.IO;
 
 public class CardEditor : EditorWindow
 {
     [SerializeField] private VisualTreeAsset m_TabbedMenuTree;
+
     private TabbedMenuController m_Controller;
+
+    protected string m_LevelUpsPath = "Assets/ScriptableObjects/Cards/LevelUps";
 
     [MenuItem("Window/CardEditor")]
     public static void ShowCardEditr()
@@ -18,6 +25,7 @@ public class CardEditor : EditorWindow
 
     public void CreateGUI()
     {
+        CreateTypes();
         ShowTabbedMenu();
     }
 
@@ -25,5 +33,33 @@ public class CardEditor : EditorWindow
     {
         rootVisualElement.Add(m_TabbedMenuTree.CloneTree());
         m_Controller = new(rootVisualElement.Q<VisualElement>("TabbedMenu"));
+    }
+
+    private void CreateTypes()
+    {
+        CreateLevelUpsDirectory();
+        string[] typeNames = Enum.GetNames(typeof(CardTypeEnum));
+
+        foreach (var name in typeNames)
+        {
+            var asset = AssetDatabase.LoadAssetAtPath<LevelUps>(m_LevelUpsPath + $"/{name}.asset");
+            
+            if (asset == null)
+            {
+                var newOne = CreateInstance<LevelUps>();
+                newOne.name = name;
+                newOne.Init();
+                AssetDatabase.CreateAsset(newOne, m_LevelUpsPath + $"/{name}.asset");
+            }
+        }
+    }
+
+    private void CreateLevelUpsDirectory()
+    {
+        if(!Directory.Exists(m_LevelUpsPath))
+        {
+            Directory.CreateDirectory(m_LevelUpsPath);
+            AssetDatabase.Refresh();
+        }
     }
 }
